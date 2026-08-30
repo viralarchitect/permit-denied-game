62ec7c41b044f9bc468ed7aea8bfce5d3e2f7061
viralarchitect <449071+viralarchitect@users.noreply.github.com>
2026-08-29 19:38:29 -0500

    feat(assets): stamp boom frames and smash wavs
    Add cmd/genfx to write boom_00..05, spark_00..03, and PCM wreck/peel/burst
    WAVs. Embed usable/sfx/*.wav. Re-run with go run ./cmd/genfx.

--- assets/embed.go
+++ assets/embed.go
@@ -2,5 +2,5 @@ package assets
 
 import "embed"
 
-//go:embed usable/lot.json usable/tileset.png usable/sprites.png usable/sprites.json
+//go:embed usable/lot.json usable/tileset.png usable/sprites.png usable/sprites.json usable/sfx/*.wav
 var FS embed.FS
